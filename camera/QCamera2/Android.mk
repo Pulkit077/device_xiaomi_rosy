77f41c838a02744c91404ed56f39abf920550d66
Jiyong Park <jiyong@google.com>
2017-06-29 15:48:22 +0900

    rosy: Build camera.* with BOARD_VNDK_VERSION
    Instead of relying on the global include paths, use the header
    libraries. Global include paths are not available when building with
    BOARD_VNDK_VERSION. pthread.h is included explicitly since it isn't
    automatically available then.
    
    Bug: 37342627
    Test: BOARD_VNDK_VERSION=current m -j camera.msm8996
    
    Change-Id: I210629684ff3451512e2dc4391f6718d5ebdd19b

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -132,6 +132,7 @@ endif
 ifeq ($(TARGET_TS_MAKEUP),true)
 LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
+LOCAL_HEADER_LIBRARIES += media_plugin_headers
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
