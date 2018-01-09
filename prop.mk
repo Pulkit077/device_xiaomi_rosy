6d760b776fbe814d9f4e5f1600f40359926cd070
Suren Baghdasaryan <surenb@google.com>
2018-01-09 15:47:40 -0800

    rosy: Set lmkd configuration properties
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- prop.mk
+++ prop.mk
@@ -112,6 +112,19 @@ persist.gps.qc_nlp_in_use=1 \
 persist.loc.nlp_name=com.qualcomm.location \
 ro.gps.agps_provider=1
 
+# LMKD
+PRODUCT_PRODUCT_PROPERTIES += \
+ro.lmk.low=1001 \
+ro.lmk.medium=0 \
+ro.lmk.critical=0 \
+ro.lmk.critical_upgrade=false \
+ro.lmk.upgrade_pressure=100 \
+ro.lmk.downgrade_pressure=100 \
+ro.lmk.kill_heaviest_task=true \
+ro.lmk.kill_timeout_ms=100 \
+ro.lmk.use_minfree_levels=true \
+ro.lmk.log_stats=true
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 av.debug.disable.pers.cache=1 \
