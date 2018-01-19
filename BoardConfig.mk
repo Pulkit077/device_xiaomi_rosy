3f02b3da578982b0a3bbc528c2d4f40a6208bd68
Pranav Vashi <neobuddy89@gmail.com>
2018-01-20 00:27:08 +0530

    rosy: Compile libhwui in perf mode
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,6 +156,9 @@ TARGET_NO_RPC := true
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_rosy
 TARGET_RECOVERY_DEVICE_MODULES := libinit_rosy
