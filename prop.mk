7de856637c7d37ee404fc2070693f6215a0eb204
Ramjee Singh <ramjee@codeaurora.org>
2018-07-26 14:59:19 +0530

    rosy: audio: configs: Enable h/w memory tuning
    H/w binder reserves 1 MB by default for memory operations.
    Customization is to only reserve usable memory
    
    Change-Id: Ide08e72e4437da270d498ff30b3cc1640d39d963

--- prop.mk
+++ prop.mk
@@ -12,6 +12,7 @@ audio.offload.video=true \
 persist.vendor.audio.fluence.speaker=true \
 persist.vendor.audio.fluence.voicecall=true \
 persist.vendor.audio.fluence.voicerec=false \
+persist.vendor.audio.hw.binder.size_kbyte=1024 \
 ro.config.media_vol_steps=25 \
 ro.config.vc_call_vol_steps=7 \
 ro.vendor.audio.sdk.fluencetype=fluence \
