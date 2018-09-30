4436220553062f5788572856494a3fd3e83881cb
muralivijay <muralidharan9845@gmail.com>
2018-09-30 13:43:39 +0530

    rosy: build android.hardware.ir HAL

--- device.mk
+++ device.mk
@@ -132,6 +132,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
 
+# ConsumerIr
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
