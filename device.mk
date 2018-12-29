1889587eaf39613e3d9d41b82995d9492d5821ed
IsaacChen <tingyi364@gmail.com>
2018-12-29 09:23:17 +0530

    rosy: Blacklist FP event
    * After screen unlocked,blacklist FP event to avoid some misbehavior.

--- device.mk
+++ device.mk
@@ -212,6 +212,11 @@ PRODUCT_PACKAGES += \
     ims-ext-common \
     ims_ext_common.xml
 
+# IDC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
+    $(LOCAL_PATH)/idc/uinput-goodix.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-goodix.idc
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5x06_ts.kl \
