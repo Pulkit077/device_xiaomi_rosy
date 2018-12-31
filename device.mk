dba22e9e19135ce0b49b28aac212ef076736d84e
haran <harangovindan@gmail.com>
2018-12-31 11:22:17 +0000

    rosy: camera: Import OSS Camera HAL
    * LA.UM.5.6.r1-05900-89xx.0

--- device.mk
+++ device.mk
@@ -123,7 +123,10 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
     libmm-qcamera \
-    Snap
+    Snap \
+    camera.device@3.2-impl \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Configstore
 PRODUCT_PACKAGES += \
