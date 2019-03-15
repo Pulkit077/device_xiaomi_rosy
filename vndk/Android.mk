118d8623f462c7af5be119682223d28f484bbd3d
dianlujitao <dianlujitao@lineageos.org>
2019-03-15 14:05:20 +0800

    rosy: Remove fake android.hidl.manager@1.0.so

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -41,7 +41,6 @@ VNDK_SP_LIBRARIES := \
 
 EXTRA_VENDOR_LIBRARIES := \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0 \
     vendor.display.config@1.0 \
     vendor.display.config@1.1 \
     vendor.display.config@1.2 \
