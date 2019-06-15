BealeStBluesBoy/android_device_xiaomi_msm8953-common

msm8953-common: Replace GloveMode class with Touch HAL

touch/GloveMode.cpp
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+
+#include "GloveMode.h"
+
+namespace vendor {
+namespace lineage {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+constexpr const char kControlPath[] = "/sys/class/tp_glove/device/glove_enable";
+
+GloveMode::GloveMode() {
+    mHasGloveMode = !access(kControlPath, F_OK);
+}
+
+// Methods from ::vendor::lineage::touch::V1_0::IGloveMode follow.
+Return<bool> GloveMode::isEnabled() {
+    std::string buf;
+
+    if (!mHasGloveMode) return false;
+
+    if (!android::base::ReadFileToString(kControlPath, &buf, true)) {
+        LOG(ERROR) << "Failed to read " << kControlPath;
+        return false;
+    }
+
+    return std::stoi(android::base::Trim(buf)) == 1;
+}
+
+Return<bool> GloveMode::setEnabled(bool enabled) {
+    if (!mHasGloveMode) return false;
+
+    if (!android::base::WriteStringToFile((enabled ? "1" : "0"), kControlPath, true)) {
+        LOG(ERROR) << "Failed to write " << kControlPath;
+        return false;
+    }
+
+    return true;
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor

touch/GloveMode.h
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VENDOR_LINEAGE_TOUCH_V1_0_GLOVEMODE_H
+#define VENDOR_LINEAGE_TOUCH_V1_0_GLOVEMODE_H
+
+#include <vendor/lineage/touch/1.0/IGloveMode.h>
+
+namespace vendor {
+namespace lineage {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+using ::android::hardware::Return;
+
+class GloveMode : public IGloveMode {
+  public:
+    GloveMode();
+
+    // Methods from ::vendor::lineage::touch::V1_0::IGloveMode follow.
+    Return<bool> isEnabled() override;
+    Return<bool> setEnabled(bool enabled) override;
+
+  private:
+    bool mHasGloveMode;
+};
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor
+
+#endif  // VENDOR_LINEAGE_TOUCH_V1_0_GLOVEMODE_H

touch/service.cpp
@@ -20,6 +20,7 @@
 #include <hidl/HidlTransportSupport.h>
 
 #include "KeyDisabler.h"
+#include "GloveMode.h"
 
 using android::OK;
 using android::sp;
@@ -29,9 +30,12 @@ using android::hardware::joinRpcThreadpool;
 
 using ::vendor::lineage::touch::V1_0::IKeyDisabler;
 using ::vendor::lineage::touch::V1_0::implementation::KeyDisabler;
+using ::vendor::lineage::touch::V1_0::IGloveMode;
+using ::vendor::lineage::touch::V1_0::implementation::GloveMode;
 
 int main() {
     sp<KeyDisabler> keyDisabler;
+    sp<GloveMode> gloveMode;
     status_t status;
 
     LOG(INFO) << "Touch HAL service is starting.";
@@ -42,11 +46,24 @@ int main() {
         goto shutdown;
     }
 
+    gloveMode = new GloveMode();
+    if (gloveMode == nullptr) {
+        LOG(ERROR) << "Can not create an instance of Touch HAL GloveMode Iface, exiting.";
+        goto shutdown;
+    }
+
     configureRpcThreadpool(1, true /*callerWillJoin*/);
 
     status = keyDisabler->registerAsService();
     if (status != OK) {
         LOG(ERROR) << "Could not register service for Touch HAL KeyDisabler Iface ("
+                   << status << ")";
+        goto shutdown;
+    }
+
+    status = gloveMode->registerAsService();
+    if (status != OK) {
+        LOG(ERROR) << "Could not register service for Touch HAL GloveMode Iface ("
                    << status << ")";
         goto shutdown;
     }

Pulkit077/device_xiaomi_rosy

Change-Id: I0cf6154db0699707cf94f5c1989695645c47b0c7

touch/GloveMode.cpp
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+
+#include "GloveMode.h"
+
+namespace vendor {
+namespace mokee {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+constexpr const char kControlPath[] = "/sys/class/tp_glove/device/glove_enable";
+
+GloveMode::GloveMode() {
+    mHasGloveMode = !access(kControlPath, F_OK);
+}
+
+// Methods from ::vendor::mokee::touch::V1_0::IGloveMode follow.
+Return<bool> GloveMode::isEnabled() {
+    std::string buf;
+
+    if (!mHasGloveMode) return false;
+
+    if (!android::base::ReadFileToString(kControlPath, &buf, true)) {
+        LOG(ERROR) << "Failed to read " << kControlPath;
+        return false;
+    }
+
+    return std::stoi(android::base::Trim(buf)) == 1;
+}
+
+Return<bool> GloveMode::setEnabled(bool enabled) {
+    if (!mHasGloveMode) return false;
+
+    if (!android::base::WriteStringToFile((enabled ? "1" : "0"), kControlPath, true)) {
+        LOG(ERROR) << "Failed to write " << kControlPath;
+        return false;
+    }
+
+    return true;
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace mokee
+}  // namespace vendor

touch/GloveMode.h
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VENDOR_MOKEE_TOUCH_V1_0_GLOVEMODE_H
+#define VENDOR_MOKEE_TOUCH_V1_0_GLOVEMODE_H
+
+#include <vendor/mokee/touch/1.0/IGloveMode.h>
+
+namespace vendor {
+namespace mokee {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+using ::android::hardware::Return;
+
+class GloveMode : public IGloveMode {
+  public:
+    GloveMode();
+
+    // Methods from ::vendor::mokee::touch::V1_0::IGloveMode follow.
+    Return<bool> isEnabled() override;
+    Return<bool> setEnabled(bool enabled) override;
+
+  private:
+    bool mHasGloveMode;
+};
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace mokee
+}  // namespace vendor
+
+#endif  // VENDOR_MOKEE_TOUCH_V1_0_GLOVEMODE_H

touch/service.cpp
@@ -20,6 +20,7 @@
 #include <hidl/HidlTransportSupport.h>
 
 #include "KeyDisabler.h"
+#include "GloveMode.h"
 
 using android::OK;
 using android::sp;
@@ -29,9 +30,12 @@ using android::hardware::joinRpcThreadpool;
 
 using ::vendor::mokee::touch::V1_0::IKeyDisabler;
 using ::vendor::mokee::touch::V1_0::implementation::KeyDisabler;
+using ::vendor::mokee::touch::V1_0::IGloveMode;
+using ::vendor::mokee::touch::V1_0::implementation::GloveMode;
 
 int main() {
     sp<KeyDisabler> keyDisabler;
+    sp<GloveMode> gloveMode;
     status_t status;
 
     LOG(INFO) << "Touch HAL service is starting.";
@@ -42,11 +46,24 @@ int main() {
         goto shutdown;
     }
 
+    gloveMode = new GloveMode();
+    if (gloveMode == nullptr) {
+        LOG(ERROR) << "Can not create an instance of Touch HAL GloveMode Iface, exiting.";
+        goto shutdown;
+    }
+
     configureRpcThreadpool(1, true /*callerWillJoin*/);
 
     status = keyDisabler->registerAsService();
     if (status != OK) {
         LOG(ERROR) << "Could not register service for Touch HAL KeyDisabler Iface ("
+                   << status << ")";
+        goto shutdown;
+    }
+
+    status = gloveMode->registerAsService();
+    if (status != OK) {
+        LOG(ERROR) << "Could not register service for Touch HAL GloveMode Iface ("
                    << status << ")";
         goto shutdown;
     }