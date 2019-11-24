c6275a42d1ee94d8e74676ac8519647612f91138
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2019-11-24 15:08:34 +0530

    rosy: prop: enable 1080P on youtube

--- prop.mk
+++ prop.mk
@@ -88,7 +88,8 @@ ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
 vendor.display.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=1 \
-vendor.gralloc.enable_fb_ubwc=1
+vendor.gralloc.enable_fb_ubwc=1 \
+sys.display-size=1920x1080
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
