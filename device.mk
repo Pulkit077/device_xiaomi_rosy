247cabb776cd58e4463da54e5cb1f9965b6925ad
dianlujitao <dianlujitao@lineageos.org>
2020-02-02 16:30:50 +0800

    rosy: Build vendor variant libqti_vndfwk_detect

--- device.mk
+++ device.mk
@@ -173,7 +173,8 @@ PRODUCT_PACKAGES += \
 
 # Fwk-detect
 PRODUCT_PACKAGES += \
-    libqti_vndfwk_detect
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
