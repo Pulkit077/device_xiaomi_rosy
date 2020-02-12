fdb1373b39cc5b1349d8eea5d288b09d0ccfb005
David Ng <dave@codeaurora.org>
2020-02-11 18:54:03 -0800

    msm8953-common: config.fs: Add sys_boot cap to peripheral manager
    Enable sys_boot capability to allow system reboot
    to peripheral manager.
    
    Change-Id: I04b67543c4d2e541b9699b6faa3337b2638b3257
    (cherry picked from commit d644c68eac7c613f54e75d8c7b025b590ee63514)

--- config.fs
+++ config.fs
@@ -47,7 +47,7 @@ caps: BLOCK_SUSPEND NET_ADMIN
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: NET_BIND_SERVICE
+caps: NET_BIND_SERVICE SYS_BOOT
 
 [system/vendor/bin/pm-service]
 mode: 0755
