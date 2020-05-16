4b5a2ae00976d887b8a7ab8ed68db466acd485d1
LuK1337 <priv.luk@gmail.com>
2020-05-16 20:28:31 +0200

    rosy: Build 64-bit variants android.hardware.camera.{common,device,provider}
     E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.provider@2.4.so" not found: needed by /system/product/lib64/lib-imsvideocodec.so in namespace classloader-namespace-shared
     E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.device@3.4.so" not found: needed by /system/product/lib64/lib-imsvideocodec.so in namespace classloader-namespace-shared
     E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.provider@2.5.so" not found: needed by /system/product/lib64/lib-imsvideocodec.so in namespace classloader-namespace-shared
     E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.common@1.0.so" not found: needed by /system/product/lib64/lib-imsvideocodec.so in namespace classloader-namespace-shared
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- device.mk
+++ device.mk
@@ -123,8 +123,11 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5 \
     camera.msm8953 \
     libmm-qcamera \
     Snap \
