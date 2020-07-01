58022929cfc26f4b1800357f77472239131d9153
GuaiYiHu <guaiyihu@foxmail.com>
2020-07-01 13:52:20 +0800

    msm8953-common: proprietary: Switch to oss libbtconfigstore
    Change-Id: Ic4d52d8bcb40024f9607d1b2c06e342b63bcf60a
    Reviewed-on: https://mokeedev.review/c/MoKee/android_device_xiaomi_msm8953-common/+/48672
    Tested-by: 乖奕虎 <guaiyihu@foxmail.com>
    Reviewed-by: 乖奕虎 <guaiyihu@foxmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -118,6 +118,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbtconfigstore
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
