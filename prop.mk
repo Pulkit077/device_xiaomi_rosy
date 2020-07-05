f6994cbf3cb45f772ea17a8653a35c5bd8b11ca9
dianlujitao <dianlujitao@lineageos.org>
2020-07-05 09:30:50 +0200

    Revert "rosy: Enable Codec2"
    Enabled on Q by default.
    
    This reverts commit 2464d067e17c06896e29b4ad448adbd825384fd6.
    
    Change-Id: I44beba8bfefce05793f657602383a2ba75dac4f8

--- prop.mk
+++ prop.mk
@@ -122,7 +122,6 @@ ro.gps.agps_provider=1
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 av.debug.disable.pers.cache=1 \
-debug.media.codec2=2 \
 debug.stagefright.omx_default_rank.sw-audio=1 \
 debug.stagefright.omx_default_rank=0 \
 media.msm8956hw=0 \
