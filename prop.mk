c4cfe10a30d6001527aada365b3bd44c7a45ffc9
Han Wang <416810799@qq.com>
2020-07-10 23:09:26 +0200

    Revert "msm8953-common: Enable GL comp backpressure"
     * This is no-op when debug.sf.disable_backpressure=1
       is set. Just follow CAF here and remove this ricing.
    
    This reverts commit da22a2f57c6cbf5e17a90f68cf7d6ff1878d13eb
    
    Change-Id: If01753e3ba27dcde6576e2cc3f058ae5121cf773

--- prop.mk
+++ prop.mk
@@ -76,7 +76,6 @@ debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
 debug.sf.latch_unsignaled=1 \
 debug.sf.recomputecrop=0 \
-debug.sf.enable_gl_backpressure=1 \
 debug.sf.disable_backpressure=1 \
 dev.pm.dyn_samplingrate=1 \
 persist.debug.wfd.enable=1 \
