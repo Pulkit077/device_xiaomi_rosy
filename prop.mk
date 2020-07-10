4feadb74f93d0dac14fb962a6f4242a7ae99a562
Anver sadhique <akurun@codeaurora.org>
2020-07-10 16:19:43 +0530

    rosy: set higher priority to c2 than OMX
    Change-Id: Id9f1c4cf5d055db051992c619218543d90d428c0

--- prop.mk
+++ prop.mk
@@ -127,7 +127,6 @@ ro.gps.agps_provider=1
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 av.debug.disable.pers.cache=1 \
-debug.stagefright.omx_default_rank.sw-audio=1 \
 debug.stagefright.omx_default_rank=0 \
 media.msm8956hw=0 \
 media.stagefright.audio.sink=280 \
