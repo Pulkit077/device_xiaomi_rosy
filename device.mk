bceb4c022a2972acbc1fd00cbf64108d27d70a61
Erfan Abdi <erfangplus@gmail.com>
2020-07-24 20:40:32 +0430

    rosy: prepare to move to in-tree HALs
    Change-Id: I4633eca7429a537b318f0e02d8c6871140a6180b

--- device.mk
+++ device.mk
@@ -34,6 +34,9 @@ TARGET_SCREEN_WIDTH := 720
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+QCOM_SOONG_NAMESPACE := \
+    $(LOCAL_PATH)/qcom-caf
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
