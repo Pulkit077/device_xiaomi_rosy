e2c29c5c0bb5ec3be01cf2c384b8fc3791ef4372
Anver sadhique <akurun@codeaurora.org>
2020-08-10 14:59:22 +0530

    rosy: audio: configs:  Add 48khz sampling rate to spkr prot tx
    msm8953 & msm8937 adding 48khz sampling rate to spkr
    prot tx and updating pcm id for spkr prot tx
    
    Change-Id: Id94aeae317d60fbdb635dd5cd39f4d0fa066ca00

--- prop.mk
+++ prop.mk
@@ -28,6 +28,7 @@ vendor.audio.parser.ip.buffer.size=262144 \
 vendor.audio.playback.mch.downsample=true \
 vendor.audio.pp.asphere.enabled=false \
 vendor.audio.safx.pbe.enabled=true \
+vendor.audio.spkr_prot.tx.sampling_rate=48000 \
 vendor.audio.tunnel.encode=false \
 vendor.audio.use.sw.alac.decoder=true \
 vendor.audio.use.sw.ape.decoder=true \
