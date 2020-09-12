1e9cf6fbf959b20694e5bd873b006a1a7657a300
Pulkit077 <pulkitagarwal2k1@gmail.com>
2020-09-12 12:20:21 +0530

    rosy: use coral sept R build fp
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- init/init_rosy.cpp
+++ init/init_rosy.cpp
@@ -108,6 +108,6 @@ void vendor_load_properties()
     property_override("ro.product.model", "Redmi 5");
     property_override("ro.build.product", "rosy");
     property_override("ro.product.device", "rosy");
-    property_override("ro.build.description", "coral-user 10 QQ3A.200805.001 6578210 release-keys");
-    property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys");
+    property_override("ro.build.description", "coral-user 11 RP1A.200720.009 6720564 release-keys");
+    property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys");
 }
