b8cd2a0fe6fe0418bd224607ef7aada64e073997
shivatejapeddi <shivatejapeddi@gmail.com>
2020-09-23 17:37:06 +0000

    rosy: Set BPF support level
    This allows a device launched with Android O but now running
    a 4.9-P+ kernel to declare itself as eBPF capable.
    
    Fixes data usage on 4.9 kernels.
    
    Change-Id: Ib44586f519ef0c5202ab841b8a1a8890e6ec74c7

--- prop.mk
+++ prop.mk
@@ -37,6 +37,10 @@ vendor.voice.playback.conc.disabled=true \
 vendor.voice.record.conc.disabled=false \
 vendor.voice.voip.conc.disabled=true
 
+# BPF
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.kernel.ebpf.supported=true
+
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
 bluetooth.hfp.client=1 \
