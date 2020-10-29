b3dedff6c935b1d149781f0fca474f183d7705a5
LuK1337 <priv.luk@gmail.com>
2020-10-29 14:31:02 +0100

    Revert "rosy: Exclude lineage-sdk overlays from RRO"
    This reverts commit cc182e9ed7356d32d68da0685e427d6f933ffde7.
    
    Revert "rosy: Exclude Snap overlays from RRO"
    
    This reverts commit ed8b532d8f3125fdef60cd4a23b7520298d976cc.
    
    Change-Id: I12f150b13686f8b91bb63e4ee728eb4016856aa5

--- device.mk
+++ device.mk
@@ -21,9 +21,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
