6bcbe2da26c861025a9820082d1ba6ee91b732ce
Pulkit077 <pulkitagarwal2k1@gmail.com>
2020-11-15 14:58:15 +0530

    rosy: import early_boot script from LA.UM.8.6.r1-05300-89xx.0
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- device.mk
+++ device.mk
@@ -327,6 +327,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.goodix.sh \
+    init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
     init.qcom.sh \
