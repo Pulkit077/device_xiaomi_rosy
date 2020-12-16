c6ed2e1c969858892c138a694e1956508655fe00
Pulkit077 <pulkitagarwal2k1@gmail.com>
2020-12-16 13:34:47 +0530

    rosy: use redfin dec R build fp
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- init/init_rosy.cpp
+++ init/init_rosy.cpp
@@ -105,6 +105,6 @@ void vendor_load_properties()
     property_override("ro.product.model", "Redmi 5");
     property_override("ro.build.product", "rosy");
     property_override("ro.product.device", "rosy");
-    property_override("ro.build.description", "redfin-user 11 RD1A.201105.003.C1 6886399 release-keys");
-    property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ1A.201205.010 6953398 release-keys");
+    property_override_triple("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys");
 }
