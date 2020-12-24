23c0524ceaabe07e01af5e6be130af781d63ae0f
Pulkit077 <pulkitagarwal2k1@gmail.com>
2020-12-24 08:50:50 +0530

    rosy: update graphics stack from LA.UM.9.6.2.r1-03600-89xx.0
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- device.mk
+++ device.mk
@@ -336,10 +336,6 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
