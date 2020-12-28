9aa006aafc1f7ad1e4683ea41faa369284bbe802
Ethan Chen <intervigil@gmail.com>
2020-12-28 08:23:16 +0100

    rosy: Remove unnecessary netutils packages
    * These are already packaged by AOSP.

--- device.mk
+++ device.mk
@@ -277,12 +277,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libavservices_minijail.vendor
 
-# Netutils
-PRODUCT_PACKAGES += \
-    netutils-wrapper-1.0 \
-    android.system.net.netd@1.0 \
-    libandroid_net
-
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
