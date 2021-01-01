fa8bed36fd2b7ec12b13c75be453af7ec0325fcd
Chenyang Zhong <zhongcy95@gmail.com>
2021-01-01 21:29:20 +0100

    rosy: Drop soundtrigger hal
    Change-Id: I8bf82100000e1dc125e19bd2b7b7bf9af790e0eb

--- device.mk
+++ device.mk
@@ -87,7 +87,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.msm8953 \
