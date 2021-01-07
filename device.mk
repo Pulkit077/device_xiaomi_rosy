8677b5dadd27be05738d045f35360cf5178f3b7c
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-01-07 14:04:25 +0530

    rosy: build android.hardware.cameradevice@3.5 and android.hardware.camera.provider@2.6
     E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.device@3.5.so" not found: needed by /system/system_ext/lib64/lib-imsvideocodec.so in namespace classloader-namespace-shared
     E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.provider@2.6.so" not found: needed by /system/system_ext/lib64/lib-imsvideocodec.so in namespace classloader-namespace-shared
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- device.mk
+++ device.mk
@@ -131,10 +131,10 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.common@1.0 \
-    android.hardware.camera.device@3.4 \
+    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.6 \
     camera.msm8953 \
     libfui \
     libui_shim \
