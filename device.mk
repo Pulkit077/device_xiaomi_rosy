267203b32f0ffa87e80ad7a181518b1edad4e10b
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-02-08 12:04:43 +0530

    rosy: restore renderscript
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- device.mk
+++ device.mk
@@ -336,6 +336,10 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
