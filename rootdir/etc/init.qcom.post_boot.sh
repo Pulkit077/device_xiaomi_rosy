c2acfe98036b8766fe9758b921d6feddbf585f9e
Ishank Rawat <irawat@codeaurora.org>
2021-03-18 21:44:42 +0530

    rosy: init.qcom.post_boot.sh: enabled DCVS parameter for Scuba.
    Change-Id: Ibc7cc462f2d78ff2e24f617ff94e291cfbc82ebe

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -3863,7 +3863,7 @@ case "$target" in
 
         # Scuba perf/power tunings
         case "$soc_id" in
-             "441" | "473" | "474" )
+             "441" | "471" | "473" | "474" )
 
             # Quad-core device. disable core_ctl
             echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/enable
@@ -3920,18 +3920,10 @@ case "$target" in
                    echo 1600 > $cpubw/bw_hwmon/idle_mbps
                 done
 
-                for memlat in $device/*cpu*-lat/devfreq/*cpu*-lat
-                do
-                    echo "mem_latency" > $memlat/governor
-                    echo 10 > $memlat/polling_interval
-                    echo 400 > $memlat/mem_latency/ratio_ceil
-                done
+                # memlat specific settings are moved to seperate file under
+                # device/target specific folder
+                setprop vendor.dcvs.prop 1
 
-                for latfloor in $device/*cpu*-ddr-latfloor*/devfreq/*cpu-ddr-latfloor*
-                do
-                    echo "compute" > $latfloor/governor
-                    echo 10 > $latfloor/polling_interval
-                done
             done
 
             # Disable low power modes. Enable it after LPM stable
