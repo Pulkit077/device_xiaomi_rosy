c7df782ad36dc47c49f0f2eff0c1961a3382fcaa
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-05-13 09:35:42 +0530

    Revert "rosy: Remove deprecated android.hardware.thermal@1.0 HAL"
    This reverts commit a2b4fef4b0a02bbc17f7def27999f4cffa4eceb4.

--- device.mk
+++ device.mk
@@ -373,6 +373,13 @@ PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
 # Thermal
+PRODUCT_PACKAGES += \
+    thermal.msm8953
+
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
