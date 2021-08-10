3e31ccc28a1fd1ed9ab39c30cfe7db19ae02c089
Pulkit077 <pulkitagarwal2k1@gmail.com>
2021-08-10 19:03:56 +0530

    rosy: props: explicitly disable iorapd
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- prop.mk
+++ prop.mk
@@ -186,6 +186,10 @@ persist.gps.qc_nlp_in_use=1 \
 persist.loc.nlp_name=com.qualcomm.location \
 ro.gps.agps_provider=1
 
+# iorapd
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+ro.iorapd.enable=false
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 av.debug.disable.pers.cache=1 \
